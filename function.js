16afe6822ec6050d47c2c1a5efb229dc8a9dfd0a
Varis Rana <vrvarisrana123@gmail.com>
2023-01-16 05:58:08 +0530

    commitNow

--- function.js
+++ function.js
@@ -1,7 +1,10 @@
 // non - primiptive =>> Array, Object, Function
-function sayHi(){
+function sayHi(param){
     console.log("Say, Hi Now please");
+    console.log("please paly with my param now ==>", param)
+    return "return from fxn now";
 }
 // fxn call
 sayHi(10);
 sayHi("hiiiiiiiiiiiiiiiiiiiiiiiiii now")
+sayHi([1,2,3,4,5])
