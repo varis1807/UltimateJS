f8711612bb91357c6875cfbb42930f7f33f4165f
Varis Rana <vrvarisrana123@gmail.com>
2023-01-16 09:21:43 +0530

    commit

--- Arrays.js
+++ Arrays.js
@@ -3,3 +3,11 @@
 
 let array = [1,2,3,4,5];
 console.log(array);
+console.log(array.length);
+let i =0;
+while(i< array.length){
+    console.log("ele at idx", i, "is", array[i]);
+    i++;
+}
+
+//push , unshift
