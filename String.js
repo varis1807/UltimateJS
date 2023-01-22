4b6790a85a1389e450cfd89b6c7ebd7ddf34771b
Varis Rana <vrvarisrana123@gmail.com>
2023-01-22 07:04:32 +0530

    commit

---
+++ String.js
@@ -0,0 +1,10 @@
+let singleQuotes = 'single quotes ki string';
+let doubleQoutes = "double quotes ki string";
+
+console.log(singleQuotes);
+console.log(doubleQoutes);
+
+let atIdx = singleQuotes.charAt(2);
+let subStr = singleQuotes.substring(2,8);
+console.log(atIdx);
+console.log(subStr);
