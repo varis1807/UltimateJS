4e246b8816fc0eb8cdf4d286fa2d635489482e48
Varis Rana <vrvarisrana123@gmail.com>
2023-01-22 07:07:50 +0530

    commit

--- String.js
+++ String.js
@@ -8,3 +8,8 @@ let atIdx = singleQuotes.charAt(2);
 let subStr = singleQuotes.substring(2,8);
 console.log(atIdx);
 console.log(subStr);
+
+//split
+let arrStr = singleQuotes.split("i");
+arrStr.join("$");
+console.log(arrStr);
